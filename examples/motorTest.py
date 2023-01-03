98b2e082bdae1188dabf89b5e4a7a635846a99b8
biggbs <brandonsbiggs@gmail.com>
2023-01-02 19:52:11 -0700

    Added a pyproject.toml and ran black formatter on the code

--- examples/motorTest.py
+++ examples/motorTest.py
@@ -1,11 +1,12 @@
 from lib.mBot import *
+
 bot = mBot()
-#bot.startWithSerial("/dev/ttyUSB0")
+# bot.startWithSerial("/dev/ttyUSB0")
 bot.startWithHID()
-while(1):
-	bot.doMove(100,100)
-	sleep(2)
-	bot.doMove(-100,-100)
-	sleep(2)
-	bot.doMove(0,0)
-	sleep(2)
+while 1:
+    bot.doMove(100, 100)
+    sleep(2)
+    bot.doMove(-100, -100)
+    sleep(2)
+    bot.doMove(0, 0)
+    sleep(2)
